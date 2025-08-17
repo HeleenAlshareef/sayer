2cf21584a715fd2594309fe9699f37a37831f07e
Abdulrhman-a1 <Abdulrhman-a1@outlook.com>
2025-08-17 14:47:22 +0300

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:sayer/common/routing/app_router.dart.dart';
 import 'package:sayer/sayer_showroom.dart';
 
 void main() {
+  //
   //starting point
   runApp(SayerShowroom(appRouter: AppRouter()));
 }
